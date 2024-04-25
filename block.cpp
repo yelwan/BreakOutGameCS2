f90e2c7efc0e250540ff18152d007d0bc7c88e8b
yelwan <160772421+yelwan@users.noreply.github.com>
2024-04-25 23:22:36 +0200

    .

--- block.cpp
+++ block.cpp
@@ -1,6 +1,6 @@
 #include "block.h"
 
-Block::Block(qreal x, qreal y)
+Block::Block(qreal x, qreal y) // initializes game object by ascribing an image to it and position x y passed as parameters
 {
     QPixmap pix(":/images/block.png");
     setPixmap(pix.scaled(20, 20));
